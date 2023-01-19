78d313512b52b7d3112add15bbdee8cd5597dca0
rakdav <rakdav@inbox.ru>
2023-01-19 11:29:21 +0200

    Добавьте файлы проекта.

---
+++ ComplexClass/Complex.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexClass
+{
+    internal class Complex
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+
+        public Complex(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+        public override string? ToString()
+        {
+            if (B > 0)
+                return A + "+" + B + "i";
+            else if (B < 0)
+                return A + "-" + Math.Abs(B) + "i";
+            return A.ToString();
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex(c1.A + c2.A, c2.B + c2.B);
+        }
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            return new Complex(c1.A - c2.A, c2.B - c2.B);
+        }
+        public static Complex operator *(Complex c1, Complex c2)
+        {
+            return new Complex(c1.A * c2.A - c1.B * c2.B,
+                c1.A * c2.B - c2.A * c1.B);
+        }
+        public static Complex operator /(Complex c1, Complex c2)
+        {
+            return new Complex((c1.A * c2.A - c1.B * c2.B) / (c2.B * c2.B + c2.A * c2.A),
+                            (c1.A * c2.B - c2.A * c1.B) / (c2.B * c2.B + c2.A * c2.A));
+        }
+        public static bool operator ==(Complex c1, Complex c2)
+        {
+            return (c1.A == c2.A) && (c1.B==c2.B);
+        }
+        public static bool operator !=(Complex c1, Complex c2)
+        {
+            return (c1.A != c2.A)||(c1.B != c2.B);
+        }
+    }
+}
---
+++ ComplexClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using ComplexClass;
+
+Complex complex1 = new Complex(4, 7);
+Complex complex2 = new Complex(5, 8);
+Complex complex3 = complex1+complex2;
+Console.WriteLine(complex3.ToString());
+Complex complex4 = complex1 - complex2;
+Console.WriteLine(complex4.ToString());
+Complex complex5 = complex1 * complex2;
+Console.WriteLine(complex5.ToString());
+Complex complex6 = complex1 / complex2;
+Console.WriteLine(complex6.ToString());
+Console.WriteLine(complex1==complex2);
+Console.WriteLine(complex1!=complex2);
